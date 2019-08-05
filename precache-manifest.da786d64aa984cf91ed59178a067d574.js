7e7f70fa93754dcfce12b2103afe7e451b701e4a
Aravind Srinivasan <aravindsrinivasan2000@gmail.com>
2019-08-05 20:37:50 +0530

    Updates

---
+++ precache-manifest.da786d64aa984cf91ed59178a067d574.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aca9929717df748dc230cd7eb73c511",
+    "url": "/DSC-Website/index.html"
+  },
+  {
+    "revision": "71d8117483b6ad97f37a",
+    "url": "/DSC-Website/static/css/2.dbd8b1e0.chunk.css"
+  },
+  {
+    "revision": "e74f85606a2ed9372cad",
+    "url": "/DSC-Website/static/css/main.5667330e.chunk.css"
+  },
+  {
+    "revision": "71d8117483b6ad97f37a",
+    "url": "/DSC-Website/static/js/2.9f9fd51c.chunk.js"
+  },
+  {
+    "revision": "e74f85606a2ed9372cad",
+    "url": "/DSC-Website/static/js/main.7929de04.chunk.js"
+  },
+  {
+    "revision": "6d115e0a3b2d1225d001",
+    "url": "/DSC-Website/static/js/runtime~main.ef0d5252.js"
+  },
+  {
+    "revision": "32d099710e65a4dfd4c82c53f5f7e246",
+    "url": "/DSC-Website/static/media/Path111.32d09971.svg"
+  },
+  {
+    "revision": "7874af48da393453ec866fb7d98000a4",
+    "url": "/DSC-Website/static/media/Path12.7874af48.svg"
+  },
+  {
+    "revision": "db23ef8c75a834ac64fb4c2039f7dbd7",
+    "url": "/DSC-Website/static/media/Path13.db23ef8c.svg"
+  },
+  {
+    "revision": "0ff900d781ad91c6a70761eca106760a",
+    "url": "/DSC-Website/static/media/Path14.0ff900d7.svg"
+  },
+  {
+    "revision": "589ce4ce18b10893168a1c708008e72d",
+    "url": "/DSC-Website/static/media/abstract1.589ce4ce.svg"
+  },
+  {
+    "revision": "6ac71d4b4423f75ba20f383e601e73eb",
+    "url": "/DSC-Website/static/media/abstract2.6ac71d4b.svg"
+  },
+  {
+    "revision": "ceb70efafb78d7b66bed581b66e312f3",
+    "url": "/DSC-Website/static/media/abstract3.ceb70efa.svg"
+  },
+  {
+    "revision": "81633772a5e2310746ed22b2335cb8c3",
+    "url": "/DSC-Website/static/media/abstract4.81633772.svg"
+  },
+  {
+    "revision": "9117f2753c417cea17e254d022c49735",
+    "url": "/DSC-Website/static/media/android.9117f275.svg"
+  },
+  {
+    "revision": "9b6877b569a56aacd6141d5b2a035f19",
+    "url": "/DSC-Website/static/media/article.9b6877b5.svg"
+  },
+  {
+    "revision": "199dca11e6b19ecb131233491372c65c",
+    "url": "/DSC-Website/static/media/coding.199dca11.svg"
+  },
+  {
+    "revision": "4cdf0d7503a49f93f6e15313cf24e7fb",
+    "url": "/DSC-Website/static/media/flutterio-icon.4cdf0d75.svg"
+  },
+  {
+    "revision": "fe91d651053aea944bf0b57c5a6d9b63",
+    "url": "/DSC-Website/static/media/learning.fe91d651.svg"
+  },
+  {
+    "revision": "8df8656fddfaf974cb9d30c20d9e5bb9",
+    "url": "/DSC-Website/static/media/menu.8df8656f.svg"
+  },
+  {
+    "revision": "473a3c997fb2020932598b312bb490c9",
+    "url": "/DSC-Website/static/media/report.473a3c99.svg"
+  },
+  {
+    "revision": "44a2a13aff824da5366c7fbe340f4958",
+    "url": "/DSC-Website/static/media/setting.44a2a13a.svg"
+  },
+  {
+    "revision": "91f9cfdde3f4593e5a0f1853a7314884",
+    "url": "/DSC-Website/static/media/team.91f9cfdd.svg"
+  },
+  {
+    "revision": "6203ec9eedd6190a0754b426462b1882",
+    "url": "/DSC-Website/static/media/tools.6203ec9e.svg"
+  },
+  {
+    "revision": "1db898c80abfefdb86770acb923d2457",
+    "url": "/DSC-Website/static/media/vr-glasses.1db898c8.svg"
+  }
+]);
